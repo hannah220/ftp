c97876328dc2b4150509b4dac7861824452d1e86
hannah <hannah@inl.ics.keio.ac.jp>
2017-03-02 17:38:14 +0900

    first commit

---
+++ client.c
@@ -0,0 +1,585 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+//#define DATASIZE 2000
+#define DATASIZE 1024
+//#define BUFSIZE 1024
+//#define BUFSIZE 104857
+#define BUFSIZE 1024
+#define PORT 10000
+
+void quit();
+void cd();
+void pwd(int num, char *com[]);
+void dir(int num, char *com[]);
+void lpwd();
+void lcd();
+void ldir(int num, char *com[]);
+void get(int num, char *com[]);
+void put(int argc, char *argv[]);
+void send_filepath(int num, char *com[]);
+
+int sd;
+
+struct command_table {
+	char *cmd;
+	void (*func)();
+} cmd_tbl[] = {
+	{"quit", quit},
+	{"pwd", pwd},
+	{"cd", cd},
+	{"dir", dir},
+	{"lpwd", lpwd},
+	{"lcd", lcd},
+	{"ldir", ldir},
+	{"get", get},
+	{"put", send_filepath},
+	{NULL, NULL}
+};
+
+struct ftp_header {
+	uint8_t type;
+	uint8_t code;
+	uint16_t length;
+};
+
+void lcd(int num, char *com[])
+{
+	if (chdir(com[1]) < 0) {
+		perror("chdir");
+	}
+}
+
+/* send file content */
+void put(int num, char *com[])
+{
+	FILE *fp;
+	char buf[BUFSIZE];
+	char *p;
+	int i, k, n = 0, len = 0, time;
+	uint16_t length;
+	int flag = 0;
+    struct ftp_header *send_header;
+    uint8_t *send_pay;
+    uint8_t *data;
+
+	char pkt_data_sub1[sizeof(struct ftp_header) + DATASIZE];
+	struct ftp_header *send_header_sub1 = (struct ftp_header *)pkt_data_sub1;
+	char *data_sub1 = pkt_data_sub1 + sizeof(struct ftp_header);
+	char *p_sub1;
+	int n_sub1, len_sub1;
+
+	if ((fp = fopen(com[1], "r")) == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	} else {
+		/* content of the file */
+		n = fread(buf, sizeof(char), BUFSIZE, fp);
+		len = n;
+		printf("first length = %d\n", len);
+
+        if (n < BUFSIZE) {   /// filesize is under BUFSIZE
+           data = (uint8_t *)malloc(sizeof(struct ftp_header) + sizeof(uint8_t) * n);
+           send_header = (struct ftp_header *)data;
+           send_pay = data + sizeof(struct ftp_header);
+           //memcpy(send_pay, buf, n);
+           strncpy(send_pay, buf, n);
+           /* send file content */
+           send_header->type = 0x20;
+           send_header->code = 0x00;
+           send_header->length = htons(len);
+           if (send(sd, data, sizeof(struct ftp_header) + len, 0) < 0) {
+               perror("send");
+               exit(EXIT_FAILURE);
+           }
+           free(data);
+        } else {          /// filesize if over BUFSIZE
+            while (!feof(fp)) {
+                data = (uint8_t *)malloc(sizeof(struct ftp_header) + sizeof(uint8_t) * n);
+                send_header = (struct ftp_header *)data;
+                send_pay = data + sizeof(struct ftp_header);
+                //memcpy(send_pay, buf, n);
+                strncpy(send_pay, buf, n);
+                /* send file content */
+                send_header->type = 0x20;
+                send_header->code = 0x01;
+                send_header->length = htons(len);
+                if (send(sd, data, sizeof(struct ftp_header) + len, 0) < 0) {
+                    perror("send");
+                    exit(EXIT_FAILURE);
+                }
+                free(data);
+                
+                memset(buf, 0, BUFSIZE);
+                n = fread(buf, sizeof(char), BUFSIZE, fp);
+                printf("read %d bytes\n", n);
+                len += n;
+                printf("len = %d\n", len);
+                if (n <= BUFSIZE) {
+                    flag++;
+                    break;
+                }
+            }
+            /* send the rest of the file */
+            printf("last %d n bytes\n", n);
+            printf("last data: %s\n", buf);
+            data = (uint8_t *)malloc(sizeof(struct ftp_header) + sizeof(uint8_t) * n);
+            send_header = (struct ftp_header *)data;
+            send_pay = data + sizeof(struct ftp_header);
+            //memcpy(send_pay, buf, n);
+            strncpy(send_pay, buf, n);
+            /* send file content */
+            send_header->type = 0x20;
+            send_header->code = 0x00;
+            send_header->length = htons(len);
+            if (send(sd, data, sizeof(struct ftp_header) + len, 0) < 0) {
+                perror("send");
+                exit(EXIT_FAILURE);
+            }
+            free(data);
+        }
+	}
+    //free(data);
+/*
+	printf("len = %d\n", len);
+
+		data[DATASIZE - 1] = '\0';
+		printf("file contents = %s, length = %d in flag == 1\n", data, len);
+		send_header->type = 0x20;
+		send_header->code = 0x01;
+		send_header->length = htons(DATASIZE);
+		if (send(sd, pkt_data, sizeof(struct ftp_header) + len, 0) < 0) {
+			perror("send");
+			exit(EXIT_FAILURE);
+		}
+
+		while (flag == 1) {
+			char pkt_data_sub[sizeof(struct ftp_header) + DATASIZE];
+			struct ftp_header *send_header_sub = (struct ftp_header *)pkt_data_sub;
+			char *data_sub = pkt_data_sub + sizeof(struct ftp_header);
+			char *p_sub;
+			int n_sub, len_sub;
+			
+			n_sub = fread(data_sub, sizeof(char), BUFSIZE, fp);
+			len_sub = n_sub;
+			p_sub= data_sub;
+			while (!feof(fp)) {
+				for (i = 0; i < n_sub; i++) {
+					p_sub++;
+				}
+				n = fread(p_sub, sizeof(char), BUFSIZE, fp);
+				//printf("read %d bytes\n", n_sub);
+				len_sub += n_sub;
+				if (len_sub >= DATASIZE) {
+					break;
+				} else {
+					flag = 0;
+				}
+			}
+			data_sub[len_sub] = '\0';
+			printf("file contents = %s, length = %d\n", data_sub, len_sub);
+			send_header_sub->type = 0x20;
+			
+				send_header_sub->code = 0x01;
+		
+			send_header_sub->length = htons((uint16_t)len_sub);
+			if (send(sd, pkt_data_sub, sizeof(struct ftp_header) + len_sub, 0) < 0) {
+				perror("send");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		 send the rest, last message!!!!!!!!!! 
+		n_sub1 = fread(data_sub1, sizeof(char), BUFSIZE, fp);
+		len_sub1 = n_sub1;
+		p_sub1 = data_sub1;
+		while (!feof(fp)) {
+			for (i = 0; i < n_sub1; i++) {
+				p_sub1++;
+			}
+			n = fread(p_sub1, sizeof(char), BUFSIZE, fp);
+			//printf("read %d bytes\n", n_sub1);
+			len_sub1 += n_sub1;
+		}
+		data_sub1[len_sub1] = '\0';
+		printf("file contents = %s, length = %d\n", data_sub1, len_sub1);
+		send_header_sub1->type = 0x20;
+		send_header_sub1->code = 0x00;
+		send_header_sub1->length = htons((uint16_t)len_sub1);
+		if (send(sd, pkt_data_sub1, sizeof(struct ftp_header) + len_sub1, 0) < 0) {
+			perror("send");
+			exit(EXIT_FAILURE);
+		}
+	}*/
+}
+
+
+/* send file path */
+void send_filepath(int num, char *com[])
+{
+  char pkt_data[sizeof(struct ftp_header) + DATASIZE];
+  struct ftp_header *send_header = (struct ftp_header *)pkt_data;
+  char *data = pkt_data + sizeof(struct ftp_header);
+  int len;
+  struct ftp_header recv_header;
+  uint16_t data_len = 0;
+  uint8_t type, code;
+
+  len = strlen(com[1]);
+  strcpy(data, com[1]);
+  //printf("file path = %s, length = %d\n", data, len);
+
+  send_header->type = 0x06;
+  send_header->code = 0x00;
+  send_header->length = htons(len - 1);
+  
+  if (send(sd, pkt_data, sizeof(struct ftp_header) + len, 0) < 0) {
+    perror("send");
+    exit(EXIT_FAILURE);
+  }
+  /* receive only the header part */
+  if (recv(sd, &recv_header, sizeof(struct ftp_header), 0) < 0) {
+    perror("recv");
+    exit(EXIT_FAILURE);
+  }
+  /* save the header info */
+  data_len = ntohs(recv_header.length);
+  type = recv_header.type;
+  code = recv_header.code;
+
+  if (type == 0x10) {
+    put(num, com);        ////// call put function to send file content
+  }
+}
+
+void get(int num, char *com[])
+{
+	char pkt_data[sizeof(struct ftp_header) + DATASIZE];
+	struct ftp_header *send_header = (struct ftp_header *)pkt_data;
+	char *data = pkt_data + sizeof(struct ftp_header);
+	int len, data_len, n, size;
+	struct ftp_header recv_header;
+	struct ftp_header recv_header_length;
+	char file_data[DATASIZE];
+	FILE *fp;
+	uint8_t code;
+
+	puts("get called");
+	len = strlen(com[1]);
+	strcpy(data, com[1]);
+	//printf("file path = %s, length = %d\n", data, len);
+
+	send_header->type = 0x05;
+	send_header->code = 0x00;
+	send_header->length = htons(len);
+	if (send(sd, pkt_data, sizeof(struct ftp_header) + len, 0) < 0) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
+	/* receive ack from server */
+	if (recv(sd, &recv_header, sizeof(struct ftp_header), 0) < 0) {
+		perror("recv");
+		exit(EXIT_FAILURE);
+	}
+	/* receive ftp_header and get data length */
+	if (recv(sd, &recv_header_length, sizeof(struct ftp_header), 0) < 0) {
+		perror("recv");
+		exit(EXIT_FAILURE);
+	}
+	data_len = (int)ntohs(recv_header_length.length);
+	code = recv_header_length.code;
+
+	fp = fopen(data, "w");
+	/* receive the data of file and write to the file */
+	while (data_len > 0) {
+		if ((n = read(sd, file_data, DATASIZE)) > 0) {
+			file_data[n] = '\0';
+			//printf("code = %d, data = %s, n = %d\n", code, file_data, n);
+			size = fwrite(file_data, sizeof(char), n, fp);
+			//printf("size of fwrite = %d\n", size);
+			if (size < n) {
+				if (!feof(fp)) {
+					perror("fwrite");
+				}
+				break;
+			}
+			data_len = data_len - size;
+		}
+	}
+	if (code == 0x00) {
+		//puts("close the fp!!!!!!!!!!!!!!!!");
+		fclose(fp);
+	}
+}
+
+void pwd(int num, char *com[])
+{
+	char pkt_data[sizeof(struct ftp_header) + DATASIZE];
+	struct ftp_header *send_header = (struct ftp_header *)pkt_data;
+	char *data = pkt_data + sizeof(struct ftp_header);
+	int len;
+	struct ftp_header recv_header;
+	uint16_t data_len = 0;
+	uint8_t type, code;
+	char dir[DATASIZE];
+	int n;
+
+	send_header->type = 0x02;
+	send_header->code = 0x00;
+	send_header->length = 0;
+
+	if (send(sd, send_header, sizeof(struct ftp_header) + 0, 0) < 0) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
+
+	if (recv(sd, &recv_header, sizeof(struct ftp_header), 0) < 0) {
+		perror("recv");
+		exit(EXIT_FAILURE);
+	}
+	data_len = ntohs(recv_header.length);
+	type = recv_header.type;
+	code  =recv_header.code;
+
+	if ((n = read(sd, dir, data_len)) > 0) {
+		dir[n] = '\n';
+		dir[n + 1] = '\0';
+		//fwrite(data, sizeof(char), n, stdout);
+		fputs(dir, stdout);
+	}
+}
+
+void lpwd()
+{
+	char data[DATASIZE];
+	FILE *fp;
+	
+	fp = popen("pwd", "r");
+	while (fgets(data, DATASIZE - 1, fp) != NULL) {
+		fputs(data, stdout);
+	}
+	fclose(fp);
+}
+
+void ldir(int num, char *com[])
+{
+	char info[DATASIZE];
+	int result, dir_len, k;
+	struct stat st;
+	DIR *dir;
+	struct dirent *dp;
+	char *p;
+	char data[DATASIZE];
+	
+	if (num == 0) {
+		info[0] = '.';
+		info[1] = '\0';
+	} else {
+		strcpy(info, com[1]);
+	}
+
+	result = stat(info, &st);
+	if (result != 0) {
+        fprintf(stderr, "%s is not found.\n", info);
+        exit(EXIT_FAILURE);
+    }
+	if ((st.st_mode & S_IFMT) == S_IFDIR) {
+        //printf("%s is directory.\n", info);
+		if ((dir = opendir(info)) == NULL) {
+			perror("opendir");
+			exit(EXIT_FAILURE);
+		}
+		p = data;
+		for (dp = readdir(dir); dp != NULL; dp = readdir(dir)) {
+			printf("%s\n", dp->d_name);
+		}
+    } else {
+        printf("%s is not directory.\n", info);
+    }
+}
+
+void quit()
+{
+	struct ftp_header send_header;
+	send_header.type = 0x01;
+	send_header.code = 0x00;
+	send_header.length = 0;
+	if (send(sd, &send_header, sizeof(struct ftp_header), 0) < 0) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
+	close(sd);
+	exit(0);
+}
+
+void dir(int num, char *com[])
+{
+	char pkt_data[sizeof(struct ftp_header) + DATASIZE];
+	struct ftp_header *send_header = (struct ftp_header *)pkt_data;
+	char *data = pkt_data + sizeof(struct ftp_header);
+	struct ftp_header recv_header;
+	uint16_t data_len = 0;
+	uint8_t type, code;
+	char dir[DATASIZE];
+	int n, len;
+
+	//printf("num ==  %d\n", num);
+
+	if (num == 1) {
+		len = strlen(com[1]);
+		strcpy(data, com[1]);
+		//printf("file path = %s, length = %d\n", data, len);
+
+		send_header->type = 0x04;
+		send_header->code = 0x00;
+		send_header->length = htons(len);
+	} else {
+		len = 0;
+		send_header->type = 0x04;
+		send_header->code = 0x00;
+		send_header->length = htons(0);
+	}
+
+	if (send(sd, pkt_data, sizeof(struct ftp_header) + len, 0) < 0) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
+
+	if (recv(sd, &recv_header, sizeof(struct ftp_header), 0) < 0) {
+		perror("recv");
+		exit(EXIT_FAILURE);
+	}
+	data_len = ntohs(recv_header.length);
+	type = recv_header.type;
+	code  =recv_header.code;
+
+	if ((n = read(sd, dir, data_len)) > 0) {
+		dir[n] = '\n';
+		dir[n + 1] = '\0';
+		//fwrite(data, sizeof(char), n, stdout);
+		fputs(dir, stdout);
+	}
+}
+
+void cd(int num, char *com[])
+{
+	char pkt_data[sizeof(struct ftp_header) + DATASIZE];
+	struct ftp_header *send_header = (struct ftp_header *)pkt_data;
+	char *data = pkt_data + sizeof(struct ftp_header);
+	struct ftp_header recv_header;
+	uint16_t data_len = 0;
+	uint8_t type, code;
+	char dir[DATASIZE];
+	int n, len;
+
+	//len = strlen(com[1]);
+	strcpy(data, com[1]);
+	len = strlen(data);
+	//printf("data = %s, length = %d\n", data, len);
+
+	send_header->type = 0x03;
+	send_header->code = 0x00;
+	send_header->length = htons(len);
+
+	if (send(sd, pkt_data, sizeof(struct ftp_header) + len, 0) < 0) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
+	if (recv(sd, &recv_header, sizeof(struct ftp_header), 0) < 0) {
+		perror("recv");
+		exit(EXIT_FAILURE);
+	}
+	if (recv_header.type == 0x10) {
+		puts("succesfully moved the directory in server");
+	}
+}
+
+
+void getargs(char *cp, char *argv[], int *argc)
+{
+  char *p;
+  int i = 1, len, k;
+  argv[0] = cp;
+  len = strlen(cp);
+  *argc = 0;
+
+  for (p = cp + 1, k = 1; k < len; p++, k++) {
+    if ((*p != ' ') && (*(p - 1) == '\0')) {
+      argv[i] = p;
+      i++;
+      (*argc)++;
+    } else if ((*p != ' ') && (*(p - 1) != ' ')) {
+      continue;
+    } else if (*p == ' '){
+      *p = '\0';
+    }
+  }
+}
+
+
+int main (int argc, char *argv[])
+{
+  char buf[100];
+  int num, n;
+  char *com[5];
+  struct sockaddr_in s_addr;
+  char *server_ip;
+  struct command_table *p;
+  fd_set readfds;
+  
+
+  server_ip = argv[1];
+
+  /* create client socket */
+  if ((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+  memset(&s_addr, 0, sizeof(struct sockaddr_in));
+  s_addr.sin_family = AF_INET;
+  s_addr.sin_port = htons(PORT);
+  s_addr.sin_addr.s_addr = inet_addr(server_ip);
+  /* connect to the server */
+  if (connect(sd, (struct sockaddr *)&s_addr, sizeof(s_addr)) < 0) {
+    perror("connect");
+    exit(EXIT_FAILURE);
+  }
+  
+  
+  for (;;) {
+    fprintf(stdout, "FTP$ ");
+    if (fgets(buf, sizeof(buf) - 1, stdin) == NULL) {
+      fprintf(stdout, "\n");
+      continue;
+    }
+    buf[strlen(buf) - 1] = '\0';
+    if (*buf == '\0')
+      continue;
+    getargs(buf, com, &num);
+    //printf("com[0] = %s\n", com[0]);
+	if (strncmp(com[0], "lcd", 3) == 0) {
+		chdir(com[1]);
+		continue;
+	}
+    for (p = cmd_tbl; p->cmd != NULL; p++) {
+      if (strcmp(com[0], p->cmd) == 0) {
+	(*p->func)(num, com);
+	break;
+      }
+    }
+    if (p->cmd == NULL)
+      fprintf(stderr, "nuknown command\n");
+  }
+}
+
---
+++ ftp_dir/server.c
@@ -0,0 +1,413 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define DATASIZE 1024
+//#define BUFSIZE 2000
+#define BUFSIZE 1024
+#define PORT 10000
+
+struct ftp_header {
+  uint8_t type;
+  uint8_t code;
+  uint16_t length;
+};
+
+FILE *exe_stor(int sd, int code, int data_len)
+{
+  char *file_path;
+  char data[DATASIZE];
+  int i = 0, size = 0, n = 0;
+  struct ftp_header send_header;
+  FILE *fp;
+  
+  /* fill out file path to char array[] */
+  while (data_len > 0) {
+    if ((n = read(sd, data, DATASIZE)) > 0) {
+      if (i == 0) {
+	file_path = data;
+	i++;
+      } else {
+	strcpy(&file_path[size], data);
+      }
+      size += n;
+      data_len = data_len - n;
+    }
+  }
+  file_path[size] = '\0';
+  //printf("file_path = %s\n", file_path);
+
+  if ((fp =fopen(file_path, "w")) != NULL) {
+    send_header.type = 0x10;
+    send_header.code = 0x02;
+  } else {
+    perror("fopen");
+    send_header.type = 0x10;
+    send_header.code = 0x01;      // not authorized to access to the file
+  }
+  send_header.length = htons(0);
+
+  if (send(sd, &send_header, sizeof(struct ftp_header), 0) < 0) {
+    perror("send");
+    exit(EXIT_FAILURE);
+  }
+  return fp;
+}
+
+FILE *receive_file(int sd, FILE *fp, int data_len, uint8_t code)
+{
+  char data[DATASIZE];
+  int n = 0, size = 0;
+
+  //printf("data_len = %d\n", data_len);
+  
+  /* receive the data of file and write to the file */
+  while (data_len > 0) {
+    if ((n = read(sd, data, DATASIZE)) > 0) {
+      data[n] = '\0';
+      //printf("code = %d, data = %s, n = %d\n", code, data, n);
+	  size = fwrite(data, sizeof(char), n, fp);
+	  //printf("size of fwrite = %d\n", size);
+      if (size < n) {
+	if (!feof(fp)) {
+	  perror("fwrite");
+	}
+	break;
+      }
+      data_len = data_len - size;
+    }
+  }
+  //printf("code = %d\n", code);
+  if (code == 0x00) {
+	  //puts("close the fp!!!!!!!!!!!!!!!!");
+    fclose(fp);
+    return NULL;
+  } else {
+    return fp;
+  }
+  return NULL;
+}
+
+////////////// send file to the client ///////////////////////////////////////
+void exe_retr(int sd, int data_len)
+{
+	struct ftp_header send_header_first;
+	char info[DATASIZE];
+	int n, i, len, flag = 0, sucess = 0, size;
+    char buf[BUFSIZE];
+	//char pkt_data[sizeof(struct ftp_header) + DATASIZE];
+	//struct ftp_header *send_header = (struct ftp_header *)pkt_data;
+	//char *data = pkt_data + sizeof(struct ftp_header);
+	FILE *fp;
+	char *p;
+    struct ftp_header *send_header;
+    uint8_t *send_pay;
+    uint8_t *data;
+
+    //data = send_header + sizeof(struct ftp_header);
+    memset(info, 0, DATASIZE);
+	if (recv(sd, info, data_len, 0) < 0) {
+		perror("recv");
+		exit(EXIT_FAILURE);
+    }
+    
+    printf("exe_retr called\n");
+	//info[n] = '\0';
+	printf("file_path = %s\n", info);
+	if ((fp = fopen(info, "r")) == NULL) {
+		perror("fopen");
+		send_header_first.type = 0x12;
+		send_header_first.code = 0x00;
+	} else {
+		sucess = 1;
+		send_header_first.type = 0x10;
+		send_header_first.code = 0x01;
+	}
+	send_header_first.length = 0;
+	if (send(sd, &send_header_first, sizeof(struct ftp_header), 0) < 0) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
+	if (sucess == 1) {
+		/* read the content of the file */
+		n = fread(buf, sizeof(char), BUFSIZE, fp);
+		len = n;
+		p = buf;
+        data = (uint8_t *)malloc(sizeof(struct ftp_header) + sizeof(uint8_t) * n);
+        send_header = (struct ftp_header *)data;
+        send_pay = data + sizeof(struct ftp_header);
+        memcpy(send_pay, buf, n);
+		while (!feof(fp)) {
+			for (i = 0; i < n; i++) {
+				p++;
+			}
+			n = fread(p, sizeof(char), BUFSIZE, fp);
+			//printf("read %d bytes\n", n);
+			len += n;
+			if (len >= DATASIZE) {
+				flag++;
+				break;
+			}
+		}
+	}
+	/* send file content */
+	if (flag == 0) {
+		data[len] = '\0';
+		//printf("file contents = %s, length = %d\n", data, len);
+		send_header->type = 0x20;
+		send_header->code = 0x00;
+		send_header->length = htons(len);
+		if (send(sd, data, sizeof(struct ftp_header) + len, 0) < 0) {
+			perror("send");
+			exit(EXIT_FAILURE);
+		}
+	}
+    free(data);
+}
+
+void exe_pwd(int sd)
+{
+	char pkt_data[sizeof(struct ftp_header) + DATASIZE];
+	struct ftp_header *send_header = (struct ftp_header *)pkt_data;
+	char *data = pkt_data + sizeof(struct ftp_header);
+	FILE *fp;
+	int len = 0;
+
+	fp = popen("pwd", "r");
+	while (fgets(data, DATASIZE -1, fp) != NULL);
+	fclose(fp);
+	len = strlen(data) - 1;
+
+	send_header->length = htons((uint16_t)len);
+	send_header->type = 0x10;
+	send_header->code = 0x00;
+	if (send(sd, pkt_data, sizeof(struct ftp_header) + len, 0) < 0) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void exe_list(int sd, int data_len)
+{
+	char pkt_data[sizeof(struct ftp_header) + DATASIZE];
+	struct ftp_header *send_header = (struct ftp_header *)pkt_data;
+	char *data = pkt_data + sizeof(struct ftp_header), *p;
+	char *file_path;
+	int i = 0, k, len, dir_len, n, size;
+	struct stat st;
+	int result;
+	DIR *dir;
+	struct dirent *dp;
+	char info[DATASIZE];
+
+	//printf("data_len = %d\n", data_len);
+
+	if (data_len > 0) {
+		/* fill out file path to char array[] */
+		if ((n = read(sd, info, data_len)) < 0) {
+			perror("read");
+			exit(EXIT_FAILURE);
+		}
+		info[n] = '\0';
+	} else {
+		info[0] = '.';
+		info[1] = '\0';
+	}
+	
+	result = stat(info, &st);
+	if (result != 0) {
+        //fprintf(stderr, "%s is not found.\n", info);
+        send_header->length = 0;
+		send_header->type = 0x12;
+		send_header->code = 0x00;
+    }
+	if ((st.st_mode & S_IFMT) == S_IFDIR) {
+        //printf("%s is directory.\n", info);
+		if ((dir = opendir(info)) == NULL) {
+			perror("opendir");
+			exit(EXIT_FAILURE);
+		}
+		p = data;
+		for (dp = readdir(dir); dp != NULL; dp = readdir(dir)) {
+			//printf("%s\n", dp->d_name);
+			strcpy(p, dp->d_name);
+			dir_len = strlen(dp->d_name);
+			for (k = 0; k < dir_len; k++) {
+				p++;		
+			}
+			*p = '\n';
+			p++;
+		}
+		*p = '\0';
+		//printf("data = %s\n;", data);
+		len = strlen(data) - 1;
+
+		send_header->length = htons((uint16_t)len);
+		send_header->type = 0x10;
+		send_header->code = 0x01;
+    } else {
+        //printf("%s is not directory.\n", info);
+		send_header->length = 0;
+		send_header->type = 0x12;
+		send_header->code = 0x01;
+    }
+	if (send(sd, pkt_data, sizeof(struct ftp_header) + len, 0) < 0) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void exe_cwd(int sd, int data_len)
+{
+	struct ftp_header *send_header;
+	char info[DATASIZE];
+	int n;
+
+	//printf("data_len = %d\n", data_len);
+	/* fill out file path to char array[] */
+	if ((n = recv(sd, info, data_len, 0)) < 0) {
+		perror("read");
+		exit(EXIT_FAILURE);
+	}
+	info[n] = '\0';
+	//printf("n = %d, strlen(info) = %d\n", n, strlen(info));
+	//printf("info = %s, length = %d\n", info, strlen(info));
+
+	if (chdir(info) < 0) {
+		//puts("failed");
+		send_header->type = 0x12;
+		if (errno == ENOENT) {
+			send_header->code = 0x00;
+		} else {
+			send_header->code = 0x10;
+		}
+	} else {
+		//printf(" chdir succesfully executed");
+		send_header->type = 0x10;
+		send_header->code = 0x00;
+	}
+	send_header->length = 0;
+
+	if (send(sd, send_header, sizeof(struct ftp_header) + 0, 0) < 0) {
+		perror("send");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void exe_quit(int sd)
+{
+	close(sd);
+	exit(0);
+}
+
+
+void execute_com(int sd)
+{
+  //char pkt_data[sizeof(ftph_header) + DATASIZE];
+  //char pkt_data1[sizeof(ftph_header) + DATASIZE];
+  struct execute_table *p;
+  struct ftp_header recv_header;
+  //char *data;
+  char data[DATASIZE];
+  uint16_t data_len = 0;
+  uint8_t type, code;
+  FILE *fp = NULL;
+
+  for (;;) {
+    /* receive only the header part */
+    if (recv(sd, &recv_header, sizeof(struct ftp_header), 0) < 0) {
+      perror("recv");
+      exit(EXIT_FAILURE);
+    }
+    /* save the header info */
+    data_len = ntohs(recv_header.length);
+    type = recv_header.type;
+    code = recv_header.code;
+
+    switch (type) {
+    case 0x01:
+      exe_quit(sd);
+      exit(0);
+    case 0x02:
+      exe_pwd(sd);
+      break;
+    case 0x03:
+		exe_cwd(sd, data_len);
+      break;
+    case 0x04:
+		exe_list(sd, data_len);
+      break;
+    case 0x05: // requested to send file to the client
+		exe_retr(sd, data_len);
+      break;
+    case 0x06:
+      fp = exe_stor(sd, code, data_len);
+      break;
+    case 0x20:
+      fp = receive_file(sd, fp, data_len, code);
+      break;
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+  int master_sock, client_sock;
+  struct sockaddr_in s_addr;
+  struct sockaddr_in c_addr;
+  int pid;
+
+  if (chdir(argv[1]) < 0) {
+	  perror("chdir");
+	  exit(EXIT_FAILURE);
+  }
+	
+  /* create master socket */
+  if ((master_sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+  /* set up master socket */
+  s_addr.sin_family = AF_INET;
+  s_addr.sin_addr.s_addr = INADDR_ANY;
+  s_addr.sin_port = htons(PORT);
+  /* bind master socket to localhost */
+  if (bind(master_sock, (struct sockaddr *)&s_addr, sizeof(s_addr)) < 0) {
+    perror("bind");
+    exit(EXIT_FAILURE);
+  }
+  /* set pending connection up to 5 */
+  if (listen(master_sock, 5) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+
+  for (;;) {
+    if ((client_sock = accept(master_sock, NULL, NULL)) < 0) {
+      perror("accept");
+      exit(EXIT_FAILURE);
+    }
+    if ((pid = fork()) < 0) {
+      perror("fork");
+      exit(EXIT_FAILURE);
+    } else if (pid == 0) {   /* child process */
+      execute_com(client_sock);
+    } else {                 /* parent process */
+      close(client_sock);
+    }
+		
+  }
+  return 0;
+}
